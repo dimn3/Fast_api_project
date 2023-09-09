4a7354e9c9c40dc82f9f24caf5c3268f98105bd0
dimn3 <parfecto@yandex.ru>
2023-09-09 14:14:13 +0300

    fix

--- app/api/endpoints/donation.py
+++ app/api/endpoints/donation.py
@@ -6,7 +6,7 @@ from app.core.user import current_superuser, current_user
 from app.crud.donation import donation_crud
 from app.models import User
 from app.schemas.donation import DonationAdmin, DonationCreate, DonationDB
-from cat_charity_fund.app.services.investment import investment
+from cat_charity_fund.app.services.investment_service import investment
 
 router = APIRouter()
 
